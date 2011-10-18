1b2802fb73983dc0086b3480c62f258f7a5ff3c3
Michael Mustun <michael.mustun@gmail.com>
2011-10-18 22:32:41 +0200

    Initial added doit.sh.

---
+++ doit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+
+outfile="machine.txt"
+ma=`uname -a`
+echo $ma > $outfile
+/Applications/a2pdf --noline-numbers --noperl-syntax $outfile -o ${outfile}.pdf
+
+
+
+cd ./perl
+
+filelist=`ls *.pl | sort`
+
+echo "-- will do these files: $filelist"
+
+for a in $filelist; do
+  echo "-- do file: $a"
+  ./_admin/_run.sh $a
+done
+
+
